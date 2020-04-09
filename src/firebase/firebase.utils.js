d5362f114215291c6a7078e30c0f134743f19523
William Smith <wpsmith.dev@gmail.com>
2020-04-09 13:52:14 -0600

    add firebase integration, sign-in page with Google sign-in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyD-3YZ-oQoIYlVwl1-qtGbnttdxUek6-F4",
+    authDomain: "crown-db-d5d76.firebaseapp.com",
+    databaseURL: "https://crown-db-d5d76.firebaseio.com",
+    projectId: "crown-db-d5d76",
+    storageBucket: "crown-db-d5d76.appspot.com",
+    messagingSenderId: "968540120907",
+    appId: "1:968540120907:web:dd2f22206b81c189332ace",
+    measurementId: "G-13C9X5R4VT"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
