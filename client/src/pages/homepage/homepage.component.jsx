5d8f952def3322e05f23cbc8a5d77aab1aea218a
William Smith <wpsmith.dev@gmail.com>
2020-05-12 22:35:16 -0600

    delete client node_modules folder

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -3,12 +3,11 @@ import React from "react";
 import Directory from "../../components/directory/directory.component";
 
 import "./homepage.styles.scss";
-import HomePageContainer from "./homepage.styles";
 
 const HomePage = () => (
-  <HomePageContainer>
+  <div className="homepage">
     <Directory />
-  </HomePageContainer>
+  </div>
 );
 
 export default HomePage;
